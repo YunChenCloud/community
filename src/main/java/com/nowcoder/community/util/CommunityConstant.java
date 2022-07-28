2355f07f66d03554949ada8020c576e2969730ba
Yuki Chen <826992207@qq.com>
2022-07-28 18:02:33 +0800

    1.增加了显示评论的功能 2.做了事务管理的测试

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -27,4 +27,14 @@ public interface CommunityConstant {
      */
     int REMEMBER_EXPIRE_SECONDS = 3600 * 24 * 100; //100天
 
+    /**
+     * 实体类型：帖子
+     */
+    int ENTITY_TYPE_POST = 1;
+
+    /**
+     * 实体类型：评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
+
 }
